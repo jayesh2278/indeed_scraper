d50390a5f6fb441725d2655a9242060991e87fd5
jayesh2278 <94175361+jayesh2278@users.noreply.github.com>
2023-03-16 08:34:55 +0530

    Update example.py

--- indeed/spiders/example.py
+++ indeed/spiders/example.py
@@ -32,26 +32,26 @@ class ExampleSpider(scrapy.Spider):
             url = response.urljoin(url)
             yield scrapy.Request(url, callback=self.details)
 
-        # next_page = response.xpath('.//*[@aria-label="Next"]/@href').extract_first()
-        # if next_page:
-        #     next_url = response.urljoin(next_page)
-        #     yield scrapy.Request(next_url, callback=self.parse)
+        next_page = response.xpath('.//*[@aria-label="Next"]/@href').extract_first()
+        if next_page:
+            next_url = response.urljoin(next_page)
+            yield scrapy.Request(next_url, callback=self.parse)
 
-            # yield scrapy.Request(url, meta={'posted_ago':posted_ago},callback=self.details)
-            # if role_csv == role_lower: 
-            #     if role_csv in self.role_check:
-            #         self.role_check[f'{role_csv}'] += 1
-            #         # print(f'\n\n\nIF ROLE_CSV',{self.role_check[f'{role_csv}']},'\n\n\n')
-            #     else:
-            #         self.role_check[f'{role_csv}'] = 0
-            #         # print('\n\n\n\nROLE CHECK',self.role_check,'\n\n\n\n')
-            #     if self.role_check[f'{role_csv}']<5: 
-                # next_page = response.xpath('.//*[@aria-label="Next"]/@href').extract_first()
-                # if next_page:
-                #     url = response.urljoin(next_page)
-                #     pp = response.xpath('.//*[@aria-label="Next"]/@data-pp').extract_first()
-                #     url = f'{url}&pp={pp}'
-                #     yield scrapy.Request(url, meta = response.meta, headers=headers, callback=self.parse)
+            yield scrapy.Request(url, meta={'posted_ago':posted_ago},callback=self.details)
+            if role_csv == role_lower: 
+                if role_csv in self.role_check:
+                    self.role_check[f'{role_csv}'] += 1
+                    # print(f'\n\n\nIF ROLE_CSV',{self.role_check[f'{role_csv}']},'\n\n\n')
+                else:
+                    self.role_check[f'{role_csv}'] = 0
+                    # print('\n\n\n\nROLE CHECK',self.role_check,'\n\n\n\n')
+                if self.role_check[f'{role_csv}']<5: 
+                next_page = response.xpath('.//*[@aria-label="Next"]/@href').extract_first()
+                if next_page:
+                    url = response.urljoin(next_page)
+                    pp = response.xpath('.//*[@aria-label="Next"]/@data-pp').extract_first()
+                    url = f'{url}&pp={pp}'
+                    yield scrapy.Request(url, meta = response.meta, headers=headers, callback=self.parse)
 
     
 
@@ -66,7 +66,7 @@ class ExampleSpider(scrapy.Spider):
         sub_job_category = None
         today = date.today()
         date_collected = today.strftime("%d/%m/%Y")
-        # posted_date = response.xpath('.//*[contains(text(),"Posted")]/following::b[1]/text()').extract_first()
+        posted_date = response.xpath('.//*[contains(text(),"Posted")]/following::b[1]/text()').extract_first()
         posted_ago = None
         company_name = response.xpath('.//*[@class="icl-u-lg-mr--sm icl-u-xs-mr--xs"]//text()').extract_first()
         description = response.xpath('.//*[@id="jobDescriptionText"]//*//text()').extract()
@@ -86,8 +86,8 @@ class ExampleSpider(scrapy.Spider):
             salary = salary.replace(' a year',' year')
         industry = 'Fashion'
         role = response.xpath('.//h1/text()').extract_first()
-        # search_query = roles['role']        
-        # key_words = response.xpath('.//*[@name="keywords"]/@content').extract_first()
+        search_query = roles['role']        
+        key_words = response.xpath('.//*[@name="keywords"]/@content').extract_first()
         domain = None
         
         employment_type = response.xpath('.//*[@class="jobsearch-JobMetadataHeader-item  icl-u-xs-mt--xs"]/text()[2]').extract_first()
@@ -103,162 +103,162 @@ class ExampleSpider(scrapy.Spider):
         else:
             # inspect_response(response, self)
              experience = None
-        # hard_skill = open('./../../../hard-skills.csv', 'r')
-        # hard_data = list(csv.reader(hard_skill))
-        # # open soft-skills csv file
-        # soft_skill = open('./../../../soft-skills.csv', 'r')
-        # soft_data = list(csv.reader(soft_skill))
-        # hard_skill1 = open('./../../../updated_hard_skills.csv', 'r')
-        # hard_data1 = list(csv.reader(hard_skill1))
-        # # open compentency-skills csv file
-        # compentency_skill = open('./../../../compentency.csv', 'r')
-        # compentency_data = list(csv.reader(compentency_skill))
-        # hard_skills = []
-        # soft_skills = []
-        # compentency_skills = []
-        # hard_skills1 = []
-        # if what_learn:
-        #     for one_line in what_learn:
-        #         punctuations = '''!()-[]{};:'"<>./?@#$%^&*_~・'''
-        #         no_punct = ""
-        #         for char in one_line:
-        #             if char not in punctuations:
-        #                 no_punct = no_punct + char
-        #         no_punct = f' {no_punct} '
-        #         # code for hard-skills
-        #         for l_csv in hard_data:
-        #             char_word = l_csv[0]
-        #             char_ = char_word.lower().split(' (',1)[0]
-        #             no_punct_char_hard = ""
-        #             for char in char_:
-        #                 if char not in punctuations:
-        #                     no_punct_char_hard += char
+        hard_skill = open('./../../../hard-skills.csv', 'r')
+        hard_data = list(csv.reader(hard_skill))
+        # open soft-skills csv file
+        soft_skill = open('./../../../soft-skills.csv', 'r')
+        soft_data = list(csv.reader(soft_skill))
+        hard_skill1 = open('./../../../updated_hard_skills.csv', 'r')
+        hard_data1 = list(csv.reader(hard_skill1))
+        # open compentency-skills csv file
+        compentency_skill = open('./../../../compentency.csv', 'r')
+        compentency_data = list(csv.reader(compentency_skill))
+        hard_skills = []
+        soft_skills = []
+        compentency_skills = []
+        hard_skills1 = []
+        if what_learn:
+            for one_line in what_learn:
+                punctuations = '''!()-[]{};:'"<>./?@#$%^&*_~・'''
+                no_punct = ""
+                for char in one_line:
+                    if char not in punctuations:
+                        no_punct = no_punct + char
+                no_punct = f' {no_punct} '
+                # code for hard-skills
+                for l_csv in hard_data:
+                    char_word = l_csv[0]
+                    char_ = char_word.lower().split(' (',1)[0]
+                    no_punct_char_hard = ""
+                    for char in char_:
+                        if char not in punctuations:
+                            no_punct_char_hard += char
 
-        #             char_ = f' {no_punct_char_hard} '
-        #             if char_ in no_punct.lower():
-        #                 if char_word not in hard_skills:
-        #                     hard_skills.append(char_word)
+                    char_ = f' {no_punct_char_hard} '
+                    if char_ in no_punct.lower():
+                        if char_word not in hard_skills:
+                            hard_skills.append(char_word)
 
-        #         for l_csv in hard_data1:
-        #             char_word = l_csv[0]
-        #             char_ = char_word.lower().split(' (',1)[0]
-        #             no_punct_char_hard = ""
-        #             for char in char_:
-        #                 if char not in punctuations:
-        #                     no_punct_char_hard += char
+                for l_csv in hard_data1:
+                    char_word = l_csv[0]
+                    char_ = char_word.lower().split(' (',1)[0]
+                    no_punct_char_hard = ""
+                    for char in char_:
+                        if char not in punctuations:
+                            no_punct_char_hard += char
 
-        #             char_ = f' {no_punct_char_hard} '
-        #             if char_ in no_punct.lower():
-        #                 if char_word not in hard_skills1:
-        #                     hard_skills1.append(char_word)
+                    char_ = f' {no_punct_char_hard} '
+                    if char_ in no_punct.lower():
+                        if char_word not in hard_skills1:
+                            hard_skills1.append(char_word)
 
-        #         # code for soft-skills            
-        #         for l_csv in soft_data:
-        #             char_word = l_csv[0]
-        #             char_ = char_word.lower().split(' (',1)[0]
-        #             no_punct_char_soft = ""
-        #             for char in char_:
-        #                 if char not in punctuations:
-        #                     no_punct_char_soft += char
-        #             char_ = f' {no_punct_char_soft} '
-        #             if char_ in no_punct.lower():
-        #                 if char_word not in soft_skills:
-        #                     soft_skills.append(char_word)
+                # code for soft-skills            
+                for l_csv in soft_data:
+                    char_word = l_csv[0]
+                    char_ = char_word.lower().split(' (',1)[0]
+                    no_punct_char_soft = ""
+                    for char in char_:
+                        if char not in punctuations:
+                            no_punct_char_soft += char
+                    char_ = f' {no_punct_char_soft} '
+                    if char_ in no_punct.lower():
+                        if char_word not in soft_skills:
+                            soft_skills.append(char_word)
 
-        #         # code for compentency-skills
-        #         for l_csv in compentency_data:
-        #             char_word = l_csv[0]
-        #             char_ = char_word.lower().split(' (',1)[0]
-        #             no_punct_char_hard = ""
-        #             for char in char_:
-        #                 if char not in punctuations:
-        #                     no_punct_char_hard += char
+                # code for compentency-skills
+                for l_csv in compentency_data:
+                    char_word = l_csv[0]
+                    char_ = char_word.lower().split(' (',1)[0]
+                    no_punct_char_hard = ""
+                    for char in char_:
+                        if char not in punctuations:
+                            no_punct_char_hard += char
 
-        #             char_ = f' {no_punct_char_hard} '
-        #             if char_ in no_punct.lower():
-        #                 if char_word not in compentency_skills:
-        #                     compentency_skills.append(char_word)
+                    char_ = f' {no_punct_char_hard} '
+                    if char_ in no_punct.lower():
+                        if char_word not in compentency_skills:
+                            compentency_skills.append(char_word)
 
-        # if role:
-        #     for one_line in [role]:
-        #         punctuations = '''!()-[]{};:'"<>./?@#$%^&*_~・'''
-        #         no_punct = ""
-        #         for char in one_line:
-        #             if char not in punctuations:
-        #                 no_punct = no_punct + char
-        #         no_punct = f' {no_punct} '
-        #         # code for hard-skills
-        #         for l_csv in hard_data:
-        #             char_word = l_csv[0]
-        #             char_ = char_word.lower().split(' (',1)[0]
-        #             no_punct_char_hard = ""
-        #             for char in char_:
-        #                 if char not in punctuations:
-        #                     no_punct_char_hard += char
+        if role:
+            for one_line in [role]:
+                punctuations = '''!()-[]{};:'"<>./?@#$%^&*_~・'''
+                no_punct = ""
+                for char in one_line:
+                    if char not in punctuations:
+                        no_punct = no_punct + char
+                no_punct = f' {no_punct} '
+                # code for hard-skills
+                for l_csv in hard_data:
+                    char_word = l_csv[0]
+                    char_ = char_word.lower().split(' (',1)[0]
+                    no_punct_char_hard = ""
+                    for char in char_:
+                        if char not in punctuations:
+                            no_punct_char_hard += char
 
-        #             char_ = f' {no_punct_char_hard} '
-        #             if char_ in no_punct.lower():
-        #                 if char_word not in hard_skills:
-        #                     hard_skills.append(char_word)
+                    char_ = f' {no_punct_char_hard} '
+                    if char_ in no_punct.lower():
+                        if char_word not in hard_skills:
+                            hard_skills.append(char_word)
 
-        #         for l_csv in hard_data1:
-        #             char_word = l_csv[0]
-        #             char_ = char_word.lower().split(' (',1)[0]
-        #             no_punct_char_hard = ""
-        #             for char in char_:
-        #                 if char not in punctuations:
-        #                     no_punct_char_hard += char
+                for l_csv in hard_data1:
+                    char_word = l_csv[0]
+                    char_ = char_word.lower().split(' (',1)[0]
+                    no_punct_char_hard = ""
+                    for char in char_:
+                        if char not in punctuations:
+                            no_punct_char_hard += char
 
-        #             char_ = f' {no_punct_char_hard} '
-        #             if char_ in no_punct.lower():
-        #                 if char_word not in hard_skills1:
-        #                     hard_skills1.append(char_word)
+                    char_ = f' {no_punct_char_hard} '
+                    if char_ in no_punct.lower():
+                        if char_word not in hard_skills1:
+                            hard_skills1.append(char_word)
 
-        #         # code for soft-skills            
-        #         for l_csv in soft_data:
-        #             char_word = l_csv[0]
-        #             char_ = char_word.lower().split(' (',1)[0]
-        #             no_punct_char_soft = ""
-        #             for char in char_:
-        #                 if char not in punctuations:
-        #                     no_punct_char_soft += char
-        #             char_ = f' {no_punct_char_soft} '
-        #             if char_ in no_punct.lower():
-        #                 if char_word not in soft_skills:
-        #                     soft_skills.append(char_word)
+                # code for soft-skills            
+                for l_csv in soft_data:
+                    char_word = l_csv[0]
+                    char_ = char_word.lower().split(' (',1)[0]
+                    no_punct_char_soft = ""
+                    for char in char_:
+                        if char not in punctuations:
+                            no_punct_char_soft += char
+                    char_ = f' {no_punct_char_soft} '
+                    if char_ in no_punct.lower():
+                        if char_word not in soft_skills:
+                            soft_skills.append(char_word)
 
-        #         # code for compentency-skills
-        #         for l_csv in compentency_data:
-        #             char_word = l_csv[0]
-        #             char_ = char_word.lower().split(' (',1)[0]
-        #             no_punct_char_hard = ""
-        #             for char in char_:
-        #                 if char not in punctuations:
-        #                     no_punct_char_hard += char
+                # code for compentency-skills
+                for l_csv in compentency_data:
+                    char_word = l_csv[0]
+                    char_ = char_word.lower().split(' (',1)[0]
+                    no_punct_char_hard = ""
+                    for char in char_:
+                        if char not in punctuations:
+                            no_punct_char_hard += char
 
-        #             char_ = f' {no_punct_char_hard} '
-        #             if char_ in no_punct.lower():
-        #                 if char_word not in compentency_skills:
-        #                     compentency_skills.append(char_word)
+                    char_ = f' {no_punct_char_hard} '
+                    if char_ in no_punct.lower():
+                        if char_word not in compentency_skills:
+                            compentency_skills.append(char_word)
 
 
-        # # close both csv files
-        # hard_skill.close()
-        # soft_skill.close()        
-        # compentency_skill.close()
-        # hard_skill1.close()
-        # if hard_skills:
-        #     hard_skills = ','.join(hard_skills)
-        # else:
-        #     hard_skills = None
-        # if soft_skills:
-        #     soft_skills = ','.join(soft_skills)
-        # else:
-        #     soft_skills = None
-        # if compentency_skills:
-        #     compentency_skills = ','.join(compentency_skills)
-        # else:
-        #     compentency_skills = None
+        # close both csv files
+        hard_skill.close()
+        soft_skill.close()        
+        compentency_skill.close()
+        hard_skill1.close()
+        if hard_skills:
+            hard_skills = ','.join(hard_skills)
+        else:
+            hard_skills = None
+        if soft_skills:
+            soft_skills = ','.join(soft_skills)
+        else:
+            soft_skills = None
+        if compentency_skills:
+            compentency_skills = ','.join(compentency_skills)
+        else:
+            compentency_skills = None
 
         item['Ref_JD_ID'] = None
         item['Ref_Skills_ID'] = None
